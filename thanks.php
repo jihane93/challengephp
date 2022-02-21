d1a088a4c444f7d92660b4c5d53918164220b447
Jihane ouakhamou <ouakhamouj@gmail.com>
2022-02-21 17:07:23 +0100

    thanks.php

---
+++ thanks.php
@@ -0,0 +1,5 @@
+<?php
+
+$message= "merci" . $_POST['LastName'] . " " . $_POST['FirstName'] . " de nous avoir contacté à propos de " . $_POST['Sujet'] . "." . "</br>" . "Un de nos conseiller vous contactera soit à l'adresse " . $_POST['E-Mail'] . "ou par téléphone au " . $_POST['NumberPhone'] . " dans les plus brefs délais pour traiter votre demande : " . $_POST['message'];
+
+echo $message;
