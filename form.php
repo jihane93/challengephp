08d8d8c44fb3ab98b3edaa3626420a9f6cd426b7
Jihane ouakhamou <ouakhamouj@gmail.com>
2022-02-21 23:21:53 +0100

    files modidied

--- form.php
+++ form.php
@@ -12,19 +12,19 @@
     <form action="thanks.php" method="POST">
         <div>
             <label for="name">Nom :</label>
-            <input type="text" id="name" name="FirstName">
+            <input type="text" id="name" name="FirstName"required>
         </div>
         <div>
             <label for="name">Prénom :</label>
-            <input type="text" id="name" name="LastName">
+            <input type="text" id="name" name="LastName"required>
         </div>
         <div>
             <label for="e-mail">Email :</label>
-            <input type="email" id="email" name="E-Mail">
+            <input type="email" id="email" name="E-Mail"required>
         </div>
         <div>
             <label for="phone">Phone Number :</label>
-            <input type="tel" id="phone" name="NumberPhone">
+            <input type="tel" id="phone" name="NumberPhone"required>
         </div>
         <div>
             <select name="subject" size="1">
@@ -37,16 +37,33 @@
         </div>
         <div>
             <label for="message"> Tell Us Your Message :</label>
-            <textarea id="message" name="message"></textarea>
+            <textarea id="message" name="message"required></textarea>
         </div>
         <div class="button">
             <button type="submit">Envoyer </button>
         </div>
 
     </form>
-    <?php
-var_dump($_POST);
-?>
-</body>
+    </body>
 
-</html>
+    </html>
+    <?php
+
+        if(!empty($_POST))
+        if(isset($_POST['E-Mail']) && !empty($_POST['E-Mail']) &&
+        isset($_POST['FirstName']) && !empty($_POST['FirstName']) &&
+        isset($_POST['LastName']) && !empty($_POST['LastName']) && 
+        isset($_POST['NumberPhone']) && !empty($_POST['NumberPhone']) &&
+        isset($_POST['message']) && !empty($_POST['message'])) 
+        {
+       
+        $name = strip_tags($_POST['FirstName']);
+        if(!filter_var($_POST['E-Mail'], FILTER_VALIDATE_EMAIL)){
+            die("Email incorrect !!!");
+        }
+
+
+        }else{
+        die("Le formulaire est incomplet !!!");
+        }
+?>
